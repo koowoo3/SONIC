3afa64296c134733698f6d24d200884fc1570ea7
koo <koowoo3@naver.com>
2024-10-01 09:30:47 +0000

    ubuntu version fix

---
+++ apps/mnist/bld/gcc/main.d
@@ -0,0 +1,54 @@
+main.o: /SONIC/apps/mnist/src/main.c /opt/ti/mspgcc/include/msp430.h \
+ /opt/ti/mspgcc/include/msp430fr5994.h /opt/ti/mspgcc/include/in430.h \
+ /opt/ti/mspgcc/include/iomacros.h /opt/ti/mspgcc/include/legacy.h \
+ /opt/ti/mspgcc/msp430-elf/include/stdlib.h \
+ /opt/ti/mspgcc/msp430-elf/include/machine/ieeefp.h \
+ /opt/ti/mspgcc/msp430-elf/include/_ansi.h \
+ /opt/ti/mspgcc/msp430-elf/include/newlib.h \
+ /opt/ti/mspgcc/msp430-elf/include/_newlib_version.h \
+ /opt/ti/mspgcc/msp430-elf/include/sys/config.h \
+ /opt/ti/mspgcc/msp430-elf/include/sys/features.h \
+ /opt/ti/mspgcc/lib/gcc/msp430-elf/7.3.0/include/stddef.h \
+ /opt/ti/mspgcc/msp430-elf/include/sys/reent.h \
+ /opt/ti/mspgcc/msp430-elf/include/_ansi.h \
+ /opt/ti/mspgcc/msp430-elf/include/sys/_types.h \
+ /opt/ti/mspgcc/msp430-elf/include/machine/_types.h \
+ /opt/ti/mspgcc/msp430-elf/include/machine/_default_types.h \
+ /opt/ti/mspgcc/msp430-elf/include/sys/lock.h \
+ /opt/ti/mspgcc/msp430-elf/include/sys/cdefs.h \
+ /opt/ti/mspgcc/msp430-elf/include/machine/stdlib.h \
+ /opt/ti/mspgcc/msp430-elf/include/string.h \
+ /opt/ti/mspgcc/msp430-elf/include/sys/string.h \
+ /SONIC/ext/libio/src/include/libio/console.h \
+ /opt/ti/mspgcc/msp430-elf/include/stdio.h \
+ /opt/ti/mspgcc/lib/gcc/msp430-elf/7.3.0/include/stdarg.h \
+ /opt/ti/mspgcc/msp430-elf/include/sys/types.h \
+ /opt/ti/mspgcc/msp430-elf/include/sys/_stdint.h \
+ /opt/ti/mspgcc/msp430-elf/include/machine/types.h \
+ /opt/ti/mspgcc/msp430-elf/include/sys/stdio.h \
+ /SONIC/ext/libio/src/include/libio/console_hwuart.h \
+ /SONIC/ext/libmsp/src/include/libmsp/uart.h \
+ /opt/ti/mspgcc/lib/gcc/msp430-elf/7.3.0/include/stdint.h \
+ /opt/ti/mspgcc/msp430-elf/include/stdint.h \
+ /opt/ti/mspgcc/msp430-elf/include/sys/_intsup.h \
+ /SONIC/ext/libmspbuiltins/src/include/libmspbuiltins/builtins.h \
+ /SONIC/ext/libmsp/src/include/libmsp/mem.h \
+ /SONIC/ext/libmsp/src/include/libmsp/periph.h \
+ /SONIC/ext/libmsp/src/include/libmsp/clock.h \
+ /SONIC/ext/libmsp/src/include/libmsp/watchdog.h \
+ /SONIC/ext/libmsp/src/include/libmsp/gpio.h \
+ /SONIC/ext/libalpaca/src/include/libalpaca/alpaca.h \
+ /SONIC/ext/libfixed/src/include/libfixed/fixed.h \
+ /SONIC/ext/libmat/src/include/libmat/mat.h \
+ /SONIC/ext/libdnn/src/include/libdnn/misc.h \
+ /opt/ti/mspgcc/lib/gcc/msp430-elf/7.3.0/include/stdbool.h \
+ /SONIC/ext/libdnn/src/include/libdnn/mem.h \
+ /SONIC/ext/libdnn/src/include/libdnn/state.h \
+ /SONIC/ext/libdnn/src/include/libdnn/buffer.h \
+ /SONIC/ext/libdnn/src/include/libdnn/nn.h \
+ /SONIC/ext/libdnn/src/include/libdnn/nonlinear.h \
+ /SONIC/ext/libdnn/src/include/libdnn/linalg.h \
+ /SONIC/apps/mnist/src/headers/conv1.h \
+ /SONIC/apps/mnist/src/headers/conv2.h \
+ /SONIC/apps/mnist/src/headers/fc1.h /SONIC/apps/mnist/src/headers/fc2.h \
+ /SONIC/apps/mnist/src/headers/input.h
